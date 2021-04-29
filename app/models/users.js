44c20c1246ff98587c5105e3c37ddd5de8d56b9e
FernandaST <circuitbank@MacBook-Air-de-Mac.local>
2021-04-28 19:11:00 -0500

    user model update

--- app/models/users.js
+++ app/models/users.js
@@ -3,11 +3,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: {
         require: true,
-        type: String
+        type: String,
     },
     lastname: {
         require: true,
         type: String,        
+    },
+    email: {
+        require: true,
+        type: String,
+    },
+    password: {
+        require: true,
+        type: String
     },
     age: {
         require: true,
@@ -16,14 +24,17 @@ const userSchema = new mongoose.Schema({
     gender: {
         require: true,
         type: String,
-        enum: ['Masculino', 'Femenino']
+        enum: ['Masculino', 'Femenino','Otro']
     },
     phone: {
         require: true,
         type: String
     },    
     address: [{        
-        delegation: {
+        postalCode:{
+        require: true,
+        type: Number }
+        /*delegation: {
             require: true,
             type: String
         },
@@ -34,12 +45,43 @@ const userSchema = new mongoose.Schema({
         street: {
             require: true,
             type: String
-        }
+        }*/ 
+    }],
+    garments: [{
+        usageClassification: {
+            require: true,
+            type: String,
+            enum: ['En perfecto estado', 'En buen estado','Con un detalle',"otro"]
+        },
+
+        garmentClassification: {
+            require: true,
+            type: String,
+            enum: ['Hombre', 'Mujer','Unisex']
+        },
+        garmentStyle: {
+            require: true,
+            type: String,
+     
+        },
+        garmentcolor: {
+            require: true,
+            type: String
+        },
+        size: {
+            require: true,
+            type: String
+        },
+        price: {
+            require: true,
+            type: Number,
+        },
+
     }],
     date: {
         type: Date,
         default: Date.now
-    }
+    },
 })
 
 const users = mongoose.model('users', userSchema);
